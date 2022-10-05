73dd49505bcf47c64a4951834fd4c01dbf3a5c61
ddigulio <dario.digulio@mercadolibre.com>
2022-10-05 16:28:55 -0300

    Products GetById Test

--- test/products/core/infrastructure/inMemoryProducts_test.go
+++ test/products/core/infrastructure/inMemoryProducts_test.go
@@ -1,24 +1,38 @@
 package infrastructure
 
 import (
-	products "CRUD_GO/src/products/core/infrastructure"
+	domain "CRUD_GO/src/products/core/domain"
+	infrastructure "CRUD_GO/src/products/core/infrastructure"
 	"testing"
+	"time"
 )
 
 func TestNextId(t *testing.T) {
 	got := repository.NextId()
-	want := 1
 
-	assert(t, got, want)
+	assert(t, got, 1)
 }
 
 func TestGetById(t *testing.T) {
+	repository.Add(product)
+
+	got, _ := repository.GetById(product.Id)
+
+	assert(t, got, product)
 
 }
 
-var repository = products.InMemoryProducts{}
+var repository = infrastructure.InMemoryProducts{}
+var product = domain.Product{
+	Id:          1,
+	Name:        "test",
+	Description: "some description",
+	Price:       45.23,
+	CreatedAt:   time.Now(),
+	UpdatedAt:   time.Now(),
+}
 
-func assert(t *testing.T, got int, want int) {
+func assert(t *testing.T, got any, want any) {
 	if got != want {
 		t.Errorf("NextId() = %v, want %v", got, want)
 	}
